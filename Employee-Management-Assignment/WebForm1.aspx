62b8f1100b58e75d0c851fb33265ff6a8b0b1faa
theahmed619 <theahmed619@gmail.com>
2026-08-17 21:29:02 +0530

    first commit

---
+++ Employee-Management-Assignment/WebForm1.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Employee_Management_Assignment.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Employee Management</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Employee & Department Details</h2>
+
+            <asp:Label ID="lblId" runat="server" Text="ID (For Update/Delete):"></asp:Label>&nbsp;
+            <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+            <br /><br />
+
+            <asp:Label ID="lblName" runat="server" Text="Name:"></asp:Label>&nbsp;
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+                ControlToValidate="txtName" Display="Dynamic" ErrorMessage="Enter Your Name" ForeColor="Red" ValidationGroup="CRUD">
+            </asp:RequiredFieldValidator>
+            <br /><br />
+
+            <asp:Label ID="lblAge" runat="server" Text="Age:"></asp:Label>&nbsp;
+            <asp:TextBox ID="txtAge" runat="server" TextMode="Number"></asp:TextBox>&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+                ControlToValidate="txtAge" Display="Dynamic" ErrorMessage="Enter Age" ForeColor="Red" ValidationGroup="CRUD">
+            </asp:RequiredFieldValidator>&nbsp;
+            <asp:RangeValidator ID="RangeValidator1" runat="server" 
+                ControlToValidate="txtAge" Display="Dynamic" ForeColor="Red" 
+                MinimumValue="18" MaximumValue="60" Type="Integer" ErrorMessage="Age must be 18 to 60" ValidationGroup="CRUD">
+            </asp:RangeValidator>
+            <br /><br />
+
+            <asp:Label ID="lblSalary" runat="server" Text="Salary:"></asp:Label>&nbsp;
+            <asp:TextBox ID="txtSalary" runat="server"></asp:TextBox>&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" 
+                ControlToValidate="txtSalary" Display="Dynamic" ErrorMessage="Enter Salary" ForeColor="Red" ValidationGroup="CRUD">
+            </asp:RequiredFieldValidator>
+            <br /><br />
+
+            <asp:Label ID="lblDeptName" runat="server" Text="Department Name:"></asp:Label>&nbsp;
+            <asp:TextBox ID="txtDept" runat="server"></asp:TextBox>&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" 
+                ControlToValidate="txtDept" Display="Dynamic" ErrorMessage="Enter Department" ForeColor="Red" ValidationGroup="CRUD">
+            </asp:RequiredFieldValidator>
+            <br /><br />
+
+            <asp:Button ID="btnAdd" runat="server" Text="ADD" OnClick="btnAdd_Click" ValidationGroup="CRUD" />
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" ValidationGroup="CRUD" />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" CausesValidation="false" />
+            
+            <br /><br />
+            <asp:Label ID="lblMsg" runat="server" Font-Bold="True"></asp:Label>
+            <br /><br />
+
+            <asp:GridView ID="GridView1" runat="server" Width="100%" AutoGenerateColumns="False" BorderWidth="1px" CellPadding="5">
+                <Columns>
+                    <asp:BoundField DataField="Emp_Id" HeaderText="Employee ID" />
+                    <asp:BoundField DataField="Emp_Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Emp_Age" HeaderText="Age" />
+                    <asp:BoundField DataField="Emp_Salary" HeaderText="Salary" />
+                    <asp:BoundField DataField="Dpt_Name" HeaderText="Department" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Employee-Management-Assignment/WebForm1.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Employee_Management_Assignment
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+         string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        private void BindGridView()
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = @"SELECT e.Emp_Id, e.Emp_Name, e.Emp_Age, e.Emp_Salary, d.Dpt_Name 
+                                 FROM Employee e 
+                                 LEFT JOIN Department d ON e.Emp_Id = d.Emp_Id 
+                                 ORDER BY e.Emp_Id DESC";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                con.Open();
+                using (SqlTransaction trans = con.BeginTransaction())
+                {
+                    try
+                    {
+                        // Insert into Employee table
+                        string empQuery = @"INSERT INTO Employee (Emp_Name, Emp_Age, Emp_Salary) 
+                                            VALUES (@Name, @Age, @Salary); 
+                                            SELECT SCOPE_IDENTITY();";
+
+                        SqlCommand cmdEmp = new SqlCommand(empQuery, con, trans);
+                        cmdEmp.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                        cmdEmp.Parameters.AddWithValue("@Age", Convert.ToInt32(txtAge.Text.Trim()));
+                        cmdEmp.Parameters.AddWithValue("@Salary", Convert.ToDecimal(txtSalary.Text.Trim()));
+
+                        int empId = Convert.ToInt32(cmdEmp.ExecuteScalar());
+
+                        // Insert into Department table with foreign key Emp_Id
+                        string deptQuery = "INSERT INTO Department (Emp_Id, Dpt_Name) VALUES (@EmpId, @DeptName)";
+                        SqlCommand cmdDept = new SqlCommand(deptQuery, con, trans);
+                        cmdDept.Parameters.AddWithValue("@EmpId", empId);
+                        cmdDept.Parameters.AddWithValue("@DeptName", txtDept.Text.Trim());
+                        cmdDept.ExecuteNonQuery();
+
+                        trans.Commit();
+                        lblMsg.ForeColor = System.Drawing.Color.Green;
+                        lblMsg.Text = "Record added successfully!";
+                        ClearFields();
+                        BindGridView();
+                    }
+                    catch (Exception ex)
+                    {
+                        trans.Rollback();
+                        lblMsg.ForeColor = System.Drawing.Color.Red;
+                        lblMsg.Text = "Error: " + ex.Message;
+                    }
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtId.Text))
+            {
+                lblMsg.ForeColor = System.Drawing.Color.Red;
+                lblMsg.Text = "Please enter Employee ID to update!";
+                return;
+            }
+
+            int empId = Convert.ToInt32(txtId.Text.Trim());
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                con.Open();
+                using (SqlTransaction trans = con.BeginTransaction())
+                {
+                    try
+                    {
+                        string empQuery = "UPDATE Employee SET Emp_Name = @Name, Emp_Age = @Age, Emp_Salary = @Salary WHERE Emp_Id = @EmpId";
+                        SqlCommand cmdEmp = new SqlCommand(empQuery, con, trans);
+                        cmdEmp.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                        cmdEmp.Parameters.AddWithValue("@Age", Convert.ToInt32(txtAge.Text.Trim()));
+                        cmdEmp.Parameters.AddWithValue("@Salary", Convert.ToDecimal(txtSalary.Text.Trim()));
+                        cmdEmp.Parameters.AddWithValue("@EmpId", empId);
+                        cmdEmp.ExecuteNonQuery();
+
+                        string deptQuery = "UPDATE Department SET Dpt_Name = @DeptName WHERE Emp_Id = @EmpId";
+                        SqlCommand cmdDept = new SqlCommand(deptQuery, con, trans);
+                        cmdDept.Parameters.AddWithValue("@DeptName", txtDept.Text.Trim());
+                        cmdDept.Parameters.AddWithValue("@EmpId", empId);
+                        cmdDept.ExecuteNonQuery();
+
+                        trans.Commit();
+                        lblMsg.ForeColor = System.Drawing.Color.Green;
+                        lblMsg.Text = "Record updated successfully!";
+                        ClearFields();
+                        BindGridView();
+                    }
+                    catch (Exception ex)
+                    {
+                        trans.Rollback();
+                        lblMsg.ForeColor = System.Drawing.Color.Red;
+                        lblMsg.Text = "Error: " + ex.Message;
+                    }
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtId.Text))
+            {
+                lblMsg.ForeColor = System.Drawing.Color.Red;
+                lblMsg.Text = "Please enter Employee ID to delete!";
+                return;
+            }
+
+            int empId = Convert.ToInt32(txtId.Text.Trim());
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = "DELETE FROM Employee WHERE Emp_Id = @EmpId";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@EmpId", empId);
+                    con.Open();
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        lblMsg.ForeColor = System.Drawing.Color.Green;
+                        lblMsg.Text = "Record deleted successfully!";
+                        ClearFields();
+                        BindGridView();
+                    }
+                    else
+                    {
+                        lblMsg.ForeColor = System.Drawing.Color.Red;
+                        lblMsg.Text = "Record not found!";
+                    }
+                }
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtId.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtAge.Text = string.Empty;
+            txtSalary.Text = string.Empty;
+            txtDept.Text = string.Empty;
+        }
+    }
+}
